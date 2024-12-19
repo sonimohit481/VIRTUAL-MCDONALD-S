fcffa4d4e5159a8732ea198eeb2f39df5019e067
Mohit Soni <sonimohit481@gmail.com>
2024-12-19 18:50:17 +0530

    404 page added

--- react-mc-app/src/App.tsx
+++ react-mc-app/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import { AuthProvider } from "./context/AuthContext";
-import { Auth, Cart, Home, Products } from "./screens";
+import { Auth, Cart, Home, NotFound, Products } from "./screens";
 
 export default function App() {
   return (
@@ -16,6 +16,7 @@ export default function App() {
               <Route path="/products" element={<Products />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/auth" element={<Auth />} />
+              <Route path="/404" element={<NotFound />} />
               {/* <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} /> */}
               {/* <Route
--- react-mc-app/src/main.tsx
+++ react-mc-app/src/main.tsx
@@ -2,13 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { auth } from "./config/firebase";
-
-// Check if Firebase is initialized correctly
-auth.app.options.apiKey === import.meta.env.VITE_FIREBASE_API_KEY ||
-  console.error(
-    "Firebase initialization failed. Check your environment variables."
-  );
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
