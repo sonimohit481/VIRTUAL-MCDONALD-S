ff85090c5c6f6e319db87580bcbae6718086693b
Mohit Soni <sonimohit481@gmail.com>
2024-12-29 19:12:30 +0530

    some router fixes

--- react-mc-app/src/App.tsx
+++ react-mc-app/src/App.tsx
@@ -1,38 +1,35 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
-import { AuthProvider } from "./context/AuthContext";
 import { Auth, Cart, Home, NotFound, Orders, Products } from "./screens";
 import { PrivateRoute } from "./components";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-          <Header />
+    <Router>
+      <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+        <Header />
 
-          <div className="relative overflow-hidden min-h-[calc(100vh-10vh)]  ">
-            <Routes>
-              <Route path="/" element={<Home />} />
+        <div className="relative overflow-hidden min-h-[calc(100vh-10vh)]  ">
+          <Routes>
+            <Route path="/" element={<Home />} />
 
-              <Route path="/products" element={<Products />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/auth" element={<Auth />} />
-              <Route path="/404" element={<NotFound />} />
-              <Route path="/*" element={<NotFound />} />
-              <Route
-                path="/orders"
-                element={
-                  <PrivateRoute>
-                    <Orders />
-                  </PrivateRoute>
-                }
-              />
-            </Routes>
-          </div>
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+            <Route
+              path="/orders"
+              element={
+                <PrivateRoute>
+                  <Orders />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
         </div>
-      </Router>
-    </AuthProvider>
+      </div>
+    </Router>
   );
 }
