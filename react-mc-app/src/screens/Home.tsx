54af4d09e93c55f0795beaa3115fd1866114f02c
Mohit Soni <sonimohit481@gmail.com>
2025-02-05 09:46:13 +0530

    Home screen fixes

--- react-mc-app/src/screens/Home.tsx
+++ react-mc-app/src/screens/Home.tsx
@@ -2,26 +2,21 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="relative flex justify-center items-center h-screen">
-      <div className="absolute inset-0">
-        <img
-          src="/background.png"
-          alt="Background Image"
-          className="object-cover object-center w-full h-full"
-        />
-        <div className="absolute inset-0 bg-black opacity-30"></div>
-      </div>
+    <div className="relative flex justify-center items-center h-screen bg-[url('/background.png')] bg-repeat bg-center">
+      {/* Background Overlay */}
+      <div className="absolute inset-0 bg-black opacity-20"></div>
 
-      <div className="relative z-10 text-center text-white">
-        <h1 className="text-5xl font-bold leading-tight mb-4">
+      {/* Foreground Content */}
+      <div className="relative z-10 text-center text-black px-6">
+        <h1 className="text-5xl font-bold leading-tight mb-4 [text-shadow:_1px_2px_4px_rgb(155_155_155_/_99%)]">
           Welcome to McDonald's Clone
         </h1>
-        <p className="text-lg text-white mb-8">
+        <p className="text-lg mb-6 font-semibold [text-shadow:_1px_2px_4px_rgb(155_155_155_/_99%)]">
           Discover our amazing deals and special offers!
         </p>
         <Link
           to="products"
-          className="bg-yellow-500 text-white hover:bg-yellow-400 py-2 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
+          className="bg-yellow-500 text-white hover:bg-yellow-400 py-3 px-8 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
         >
           Get Started
         </Link>
